64c776dd016e054a9206807348b98678e29ebe15
Raj Thakur <rajthakur2003@gmail.com>
2020-09-28 21:34:58 +0530

    Inital commit for our first springboot project

---
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
